0acd84414f8f52c3118718e3c7651308823f97ee
Dilalzate <129336252+Dilalzate@users.noreply.github.com>
2023-12-29 12:49:58 -0500

    Add test method to use Select object

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -23,6 +24,15 @@ public class BasicTest {
 
     driver.get(DROPDOWN_URL);
     driver.findElement(By.id("dropdown")).sendKeys("Option");
+
+  }
+
+  @Test
+  public void dropdownTestUsingSelectObject() {
+
+    driver.get(DROPDOWN_URL);
+    Select select = new Select(driver.findElement(By.id("dropdown")));
+    select.selectByVisibleText("Option 2");
 
   }
 
